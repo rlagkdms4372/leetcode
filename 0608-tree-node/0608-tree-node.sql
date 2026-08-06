f5f4b477ddf46fe46dd8e5b002d67d3c85e31bf4
김하은 (Haeun Kim) <99571254+rlagkdms4372@users.noreply.github.com>
2026-08-06 14:38:50 +0900

    Time: 496 ms (61.32%), Space: 0B (100%) - LeetHub-Neo

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT id,
+       CASE WHEN p_id IS NULL THEN 'Root' 
+            WHEN id IN (SELECT DISTINCT(p_id) FROM tree WHERE p_id IS NOT NULL) THEN 'Inner'
+            ELSE 'Leaf' END AS type
+FROM Tree
